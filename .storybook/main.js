f1b80c2fc210c2f32884766233728bcb1cfd39b4
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-09-15 20:01:33 -0300

    chore: add webpackFinal to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     '../src/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from '../src/styles/global'
-import theme from '../src/styles/theme'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
