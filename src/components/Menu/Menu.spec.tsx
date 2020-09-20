6fd8136ef903f3757204214a7ada3ff0265430a5
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-09-20 11:18:09 -0300

    test: add tests and state to Menu

--- src/components/Menu/Menu.spec.tsx
+++ src/components/Menu/Menu.spec.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Menu from 'components/Menu'
 
@@ -9,5 +10,27 @@ describe('<Menu />', () => {
     expect(getByLabelText(/search/i)).toBeInTheDocument()
     expect(getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    const { getByRole, getByLabelText } = renderWithTheme(<Menu />)
+
+    // separar em passos lógicos:
+    // selecionar o menu
+    const fullMenuElement = getByRole('navigation', { hidden: true })
+
+    //verificar se o menu ta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar no botao de abrir e verificar se ele abriu
+    fireEvent.click(getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar no botao de fechar o menu e veriticar se ele fechou
+    fireEvent.click(getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import Logo from 'components/Logo'
 import * as S from './Menu.styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile aria-label="Won Logo" />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open shopping cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile aria-label="Won Logo" />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      {/* screen read ignore with aria-hidden */}
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
