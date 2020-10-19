7461b22fc6294dac870078b181cfc6fe668001fb
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-10-19 14:42:16 -0300

    test: add test to Ribbon and onFav click

--- src/components/GameCard/GameCard.spec.tsx
+++ src/components/GameCard/GameCard.spec.tsx
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import theme from 'styles/theme'
 import GameCard from '.'
@@ -44,5 +45,46 @@ describe('<GameCard />', () => {
 
     expect(price).toHaveStyleRule('text-decoration', 'line-through')
     expect(promotionalPrice).not.toHaveStyle({ textDecoration: 'line-through' })
+  })
+
+  it('should render a filled Favorite icon when favorite is true', () => {
+    const { getByLabelText } = renderWithTheme(<GameCard {...props} favorite />)
+
+    expect(getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should call onFav method when favorite is clicked', () => {
+    const onFav = jest.fn()
+    const { getByLabelText, getAllByRole } = renderWithTheme(
+      <GameCard {...props} favorite onFav={onFav} />
+    )
+
+    fireEvent.click(getAllByRole('button')[0])
+
+    expect(onFav).toBeCalled()
+
+    expect(getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render a Ribbon', () => {
+    const { getByText } = renderWithTheme(
+      <GameCard
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3cd3c1'
+    })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
