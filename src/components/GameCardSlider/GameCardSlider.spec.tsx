6b28a8dedbb6029acdccba57ee78bafe997eaf53
Emerson Paiva <emersonaraujopaiva@gmail.com>
2020-10-20 10:00:40 -0300

    test: add test to slick arrows

--- src/components/GameCardSlider/GameCardSlider.spec.tsx
+++ src/components/GameCardSlider/GameCardSlider.spec.tsx
@@ -23,6 +23,20 @@ const items = [
     img: 'https://source.unsplash.com/user/willianjusten/300x142',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 4',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero 5',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
   }
 ]
 
@@ -30,19 +44,19 @@ describe('<GameCardSlider />', () => {
   it('should render with 4 active items', () => {
     const { container } = renderWithTheme(<GameCardSlider items={items} />)
 
-    expect(container.querySelectorAll('.slick-active')).toHaveLength(3)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
   })
 
-  // it('should render white arrows if color passed', () => {
-  //   const { getByLabelText } = renderWithTheme(
-  //     <GameCardSlider items={items} color="white" />
-  //   )
+  it('should render white arrows if color passed', () => {
+    const { getByLabelText } = renderWithTheme(
+      <GameCardSlider items={items} color="white" />
+    )
 
-  //   expect(getByLabelText(/previous games/i)).toHaveStyle({
-  //     color: '#FAFAFA'
-  //   })
-  //   expect(getByLabelText(/next games/i)).toHaveStyle({
-  //     color: '#FAFAFA'
-  //   })
-  // })
+    expect(getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 })
