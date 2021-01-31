1db761e71c644681cb9f022403790488658bd2a4
Emerson Paiva <emersonaraujopaiva@gmail.com>
2021-01-31 17:31:41 -0300

    test(Auth): update Auth tests

--- src/templates/Auth/Auth.spec.tsx
+++ src/templates/Auth/Auth.spec.tsx
@@ -4,14 +4,25 @@ import Auth from './Auth'
 describe('<Auth />', () => {
   it('should render logo, title, children', () => {
     const { getByRole, getByTestId, queryAllByLabelText } = renderWithTheme(
-      <Auth title="Title">
+      <Auth title="Auth Title">
         <span data-testid="children">Children</span>
       </Auth>
     )
 
-    expect(getByRole('heading', { name: /title/i })).toBeInTheDocument()
+    expect(
+      getByRole('heading', { name: /all your favorite games in one place/i })
+    ).toBeInTheDocument()
+
+    expect(
+      getByRole('heading', {
+        name: /won is the best and most complete gaming plataform./i
+      })
+    ).toBeInTheDocument()
+
+    expect(getByRole('heading', { name: /auth title/i })).toBeInTheDocument()
+
     expect(getByTestId('children')).toBeInTheDocument()
-    expect(getByTestId('children')).toBeInTheDocument()
+
     expect(queryAllByLabelText(/won games/i).length).toBe(2)
   })
 })
