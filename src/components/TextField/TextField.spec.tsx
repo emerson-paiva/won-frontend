07001cf0437d7f8b29dabc3949bd5818f0ab9415
Emerson Paiva <emersonaraujopaiva@gmail.com>
2021-01-31 10:27:51 -0300

    feat(TextField): add custom TextField to components

---
+++ src/components/TextField/TextField.spec.tsx
@@ -0,0 +1,55 @@
+import { waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+import TextField from './TextField'
+
+describe('<TextField />', () => {
+  it('should render with Label', () => {
+    const { queryByLabelText } = renderWithTheme(
+      <TextField labelFor="Field" label="Label" id="Field" />
+    )
+
+    expect(queryByLabelText('Label')).toBeInTheDocument()
+  })
+
+  it('should render without Label', () => {
+    const { queryByLabelText } = renderWithTheme(<TextField id="Field" />)
+
+    expect(queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('should render with Placeholder', () => {
+    const { queryByPlaceholderText } = renderWithTheme(
+      <TextField placeholder="Placeholder" />
+    )
+
+    expect(queryByPlaceholderText('Placeholder')).toBeInTheDocument()
+  })
+
+  it('should call onChange when typing value', async () => {
+    const onInput = jest.fn()
+
+    const { getByRole } = renderWithTheme(<TextField onInput={onInput} />)
+
+    expect(onInput).not.toBeCalled()
+    const input = getByRole('textbox')
+    const text = 'This is my new value'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInput).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInput).toHaveBeenCalledWith(text)
+  })
+
+  it('should be accessible with tab', () => {
+    const { getByRole } = renderWithTheme(<TextField />)
+
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+
+    expect(getByRole('textbox')).toHaveFocus()
+  })
+})
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Checkbox } from './Checkbox/Checkbox'
 export { default as Radio } from './Radio/Radio'
+export { default as TextField } from './TextField/TextField'
