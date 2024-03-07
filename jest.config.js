0ab3e88b8b6bc1f2d287d5b9a4c4196dae8d1b66
Emerson Paiva <emersonpaiva.dev@gmail.com>
2024-03-07 07:52:42 -0300

    feat: add styled-components

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,20 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**', // should be tested in e2e
+    '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/**'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  moduleNameMapper: {
+    '^styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js'
   }
 }
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import Main from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', {
+        name: /main/i
+      })
+    ).toBeInTheDocument()
+  })
+})
