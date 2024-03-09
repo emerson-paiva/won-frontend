0249f7d3038d39f8b605198bcf23a66f91cf0128
Emerson Paiva <emersonpaiva.dev@gmail.com>
2024-03-09 07:30:52 -0300

    refactor: add new props to Heading

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,19 +1,31 @@
 import * as S from './Heading.styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
   children: React.ReactNode
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 export const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+  >
     {children}
   </S.Wrapper>
 )
